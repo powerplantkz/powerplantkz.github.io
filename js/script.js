8d1dacbebfd24cb299aea6b6407113cf7aba5e54
Sergey Simonenko <serjsjc@gmail.com>
2017-02-28 02:09:08 +0500

    19

--- js/script.js
+++ js/script.js
@@ -6,8 +6,17 @@ function currentDate() {
         timezone: 'UTC'
     };
     var date = document.getElementById("currentDate").innerHTML = new Date().toLocaleString("ru", options);
+}function lastDate() {
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC'
+    };
+    var date = document.getElementById("lastDate").innerHTML = new Date().toLocaleString("ru", options);
 }
 currentDate();
+lastDate();
 
 
 
