8e51baefe8114627962b0329f47d892e2b2e7831
Sergey Simonenko <serjsjc@gmail.com>
2017-02-27 19:06:23 +0500

    8

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-document.onclick = function (e) {
-    var id = e.target.nodeName.id;
-    console.log("pressed " + id);
-};
+$(window).resize(function(){
+    var text = $(".row a"),
+        currentWidth = parseInt(text.css("width")),
+        newWidth = 28;
+
+    if(currentWidth==700) newWidth = 28;
+    else if (currentWidth<700 && currentWidth>=600) newWidth = 25;
+    else if (currentWidth<600 && currentWidth>=500) newWidth = 21;
+    else if (currentWidth<500 && currentWidth>=300) newWidth = 16;
+
+    text.css("font-size",newWidth+"px");
+});
