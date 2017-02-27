51068057c83896cdd140746cca2b0158678d3935
Sergey Simonenko <serjsjc@gmail.com>
2017-02-27 19:24:48 +0500

    9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
-$(window).resize(function(){
-    var text = $(".row a"),
-        currentWidth = parseInt(text.css("width")),
-        newWidth = 28;
-
-    if(currentWidth==700) newWidth = 28;
-    else if (currentWidth<700 && currentWidth>=600) newWidth = 25;
-    else if (currentWidth<600 && currentWidth>=500) newWidth = 21;
-    else if (currentWidth<500 && currentWidth>=300) newWidth = 16;
-
-    text.css("font-size",newWidth+"px");
-});
+var size = document.getElementById('size');
+alert(size)
