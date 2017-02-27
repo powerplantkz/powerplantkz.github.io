7c0c5f57d385748495f3fb1703f026b6a43f4360
Sergey Simonenko <serjsjc@gmail.com>
2017-02-27 21:41:33 +0500

    10

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-var size = document.getElementById('size');
-alert(size)
+function currentDate() {
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC'
+    };
+    var date = document.getElementById("size").innerHTML = new Date().toLocaleString("ru", options);
+}
+
+currentDate();
+
+
+
+
+
+
