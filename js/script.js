7b5b9e45ffa5f86b54a29ddb9d7d5e82ca1aa7c6
Sergey Simonenko <serjsjc@gmail.com>
2017-02-28 00:34:33 +0500

    14

--- js/script.js
+++ js/script.js
@@ -7,12 +7,6 @@ function currentDate() {
     };
     var date = document.getElementById("size").innerHTML = new Date().toLocaleString("ru", options);
 }
-var w = screen.width;
-
-function changeFontSize() {
-    var orientation=window.screen.lockO
-}
-window.addEventListener('resize', changeFontSize);
 currentDate();
 
 
