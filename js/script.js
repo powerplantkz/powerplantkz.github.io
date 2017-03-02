6a3b4328094f95edd1474516f2d10f08f15d55b9
Sergey Simonenko <serjsjc@gmail.com>
2017-03-02 21:08:44 +0900

    28

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.getElementById("currentDate").innerHTML = "Сегодня " + new Date().toLocaleDateString();
 var time = new Date().toLocaleTimeString();
 var dayNight = new Date().getHours();
-if (dayNight > 8 && dayNight < 20) alert("day");
-if (dayNight > 20 && dayNight < 8) alert("night");
+function () {
+    if (dayNight > 8 && dayNight < 20) alert("day");
+    if (dayNight > 20 && dayNight < 8) alert("night");
+}
 
 var A, B, C, D;
