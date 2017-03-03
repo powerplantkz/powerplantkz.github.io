78fd5e6ef89e0e74197bc6b6767b04b509a2655f
Sergey Simonenko <serjsjc@gmail.com>
2017-03-03 12:26:04 +0500

    30

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+var year = [366];
+
+var dayInYear = {
+    data: undefined,
+    crewWorkInDay: undefined,
+    crewWorkInNight: undefined,
+    item1: undefined,
+    item2: undefined,
+    item3: undefined,
+    item4: undefined
+};
+
+var currentData = new Date();
+var beginYear = new Date(currentData.getFullYear(), 0, 0);
+var diff = currentData - beginYear;
+var oneDay = 1000 * 60 * 60 * 24;
+var numberDayInYear = Math.floor(diff / oneDay);
+
+console.log("today " + numberDayInYear + " numberDayInYear in year");
+
+
 var dayNight = new Date().getHours();
 if (dayNight < 8 || dayNight > 20) dayNight = "ночь";
 if (dayNight > 8 || dayNight < 20) dayNight = "день";
-
-document.getElementById("currentDate").innerHTML = "Сегодня " + new Date().toLocaleDateString() + " в " + dayNight + " смена";
+document.getElementById("currentDate").innerHTML = "Сегодня " + new Date().toLocaleDateString() + " в " + dayNight + " смена ";
